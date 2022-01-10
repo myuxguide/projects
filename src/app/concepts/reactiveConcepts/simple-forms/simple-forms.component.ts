57ddd6612f295a11c4deccd7e06425a6b43cb508
myuxguide <myuxguide@gmail.com>
2022-01-10 14:57:52 +0530

    Added new changes

--- src/app/concepts/reactiveConcepts/simple-forms/simple-forms.component.ts
+++ src/app/concepts/reactiveConcepts/simple-forms/simple-forms.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simple-forms.component.css']
 })
 export class SimpleFormsComponent implements OnInit {
+  title:string="Simple Form in Angular"
 
   constructor() { }
 
