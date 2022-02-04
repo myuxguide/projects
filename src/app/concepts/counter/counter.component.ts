6fe7e90c201db96be369870c8aecf9d4d3a4e54a
Jimish Shah <myuxguide@gmail.com>
2022-02-04 13:02:08 +0530

    Changes

--- src/app/concepts/counter/counter.component.ts
+++ src/app/concepts/counter/counter.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent implements OnInit {
-
+  title: string = "Counter"
   constructor() { }
 
   ngOnInit(): void {
